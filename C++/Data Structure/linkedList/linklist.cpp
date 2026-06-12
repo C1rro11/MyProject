a86014e17608b7b548845fce9bd798a5a9bfa52c
CarrollCheng <carrollcheng.technine@gmail.com>
2026-06-12 09:00:51 +0800

    update

---
+++ C++/Data Structure/linkedList/linklist.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+record DoubleLinkListNode{
+    DoubleLinkListNode* next = nullptr;
+    DoubleLinkListNode* prev = nullptr;
+    int data;
+}
+
+record DoubleLinkList {
+    DoubleLinkListNode* headnode = nullptr;
+    DoubleLinkListNode* tailnode = nullptr;
+}
+
+void D
+
